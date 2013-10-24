435063d45c94c37d9194f625c39ec4cf0c8065ed
Manish Srivastava <manishsri01@gmail.com>
2013-10-25 00:22:02 +0530

    make some formating

--- src/com/manish/googleprintdemo/MainActivity.java
+++ src/com/manish/googleprintdemo/MainActivity.java
@@ -15,37 +15,44 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Toast;
+
 /**
  * 
  * @author manish
- *
+ * 
  */
 
 public class MainActivity extends Activity {
 	Button btnPrint;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		btnPrint=(Button)findViewById(R.id.button1);
-		
+		btnPrint = (Button) findViewById(R.id.button1);
+
 		btnPrint.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 
-				 if (isNetworkAvailable() == false) {
-						Toast.makeText(MainActivity.this,
-								"Network connection not available, Please try later",
-								Toast.LENGTH_SHORT).show();
-					} else {
-						File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/personal/xyz.pdf");
-						Intent printIntent = new Intent(MainActivity.this, PrintDialogActivity.class);
-						printIntent.setDataAndType(Uri.fromFile(file), "application/pdf");
-						printIntent.putExtra("title", "Android print demo");
-						startActivity(printIntent);
-					}
+				if (isNetworkAvailable() == false) {
+					Toast.makeText(
+							MainActivity.this,
+							"Network connection not available, Please try later",
+							Toast.LENGTH_SHORT).show();
+				} else {
+					File file = new File(Environment
+							.getExternalStorageDirectory().getAbsolutePath()
+							+ "/personal/xyz.pdf");
+					Intent printIntent = new Intent(MainActivity.this,
+							PrintDialogActivity.class);
+					printIntent.setDataAndType(Uri.fromFile(file),
+							"application/pdf");
+					printIntent.putExtra("title", "Android print demo");
+					startActivity(printIntent);
+				}
 			}
 		});
 	}
